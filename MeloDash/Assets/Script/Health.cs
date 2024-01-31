023f5326ad47f00f7694ef3dde91867ed7d35205
theoyveyman <theoyveyman@gmail.com>
2024-01-30 20:59:44 -0500

    Basic Health System.

---
+++ MeloDash/Assets/Script/Health.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public int healthNum;
+
+    public string damageSourceLayer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(healthNum == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Debug.Log(collision.gameObject.layer);
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer(damageSourceLayer))
+        {
+            Destroy(collision.gameObject);
+
+            healthNum--;
+
+        }
+
+    }
+}
