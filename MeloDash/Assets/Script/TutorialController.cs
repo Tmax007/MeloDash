c8b349c0aa721cd7d359689ed3843b0b6f57d7e9
theoyveyman <theoyveyman@gmail.com>
2024-03-27 10:52:25 -0400

    Tutorial controller comments.

--- MeloDash/Assets/Script/TutorialController.cs
+++ MeloDash/Assets/Script/TutorialController.cs
@@ -6,17 +6,22 @@ using UnityEngine.UI;
 
 public class TutorialController : MonoBehaviour
 {
-
+    //Integer represents the current state of the tutorial (switched to int from enum because it's easier to transition between them with the buttons).
+    //0 = dodgeTest, 1 = shootingTest, 2 = LSDTest.
     public int state;
 
     //Reference for the tutorial's enemy spawner.
     public VerticalEnemySpawner verticalEnemySpawner;
 
+    //Reference for the player.
     public GameObject player;
 
+    //Reference to multiple player functions that'll be toggled on & off throughout tutorial.
     LastSecondZone LSD;
-    GameObject LSDVisualizer;
     Shooting shoot;
+
+    //Reference to rectangle child object that's used to visualize the size of the last-second zone.
+    GameObject LSDVisualizer;
 
     public TextMeshProUGUI tutorialText;
 
@@ -39,7 +44,7 @@ public class TutorialController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //Change tutorial text if in the dodgeTest state.
         if(state == 0)
         {
             tutorialText.text = "A/left arrow key and B/right arrow key to move left and right respectively. Dodge the orbs coming from the top of the screen.";
@@ -50,7 +55,7 @@ public class TutorialController : MonoBehaviour
             prevButton.interactable = true;
         }
 
-        //Set the spawner to spawn destructible enemies if in the shootingTest state.
+        //Set the spawner to spawn destructible enemies, change tutorial text, & enable player shooting if in the shootingTest state.
         if(state == 1)
         {
             //verticalEnemySpawner.spawnDestructible = true;
@@ -58,14 +63,14 @@ public class TutorialController : MonoBehaviour
             tutorialText.text = "Space to shoot projectiles that can destroy the special orbs. Only these orbs can be destroyed.";
 
         }
-        //Set the spawner to spawn regular enemies if not in the shootingTest state.
+        //Set the spawner to spawn regular enemies & disable player shooting if not in the shootingTest state.
         else
         {
            //verticalEnemySpawner.spawnDestructible = false;
            shoot.enabled = false;
         }
 
-        //Enable the player's last-second dodging if in the lastSecondDodgeTest state.
+        //Enable the player's last-second dodging, change tutorial text, & enable the LSDVisualizer if in the LSDTest state.
         if(state == 2)
         {
             LSD.enabled = true;
@@ -74,7 +79,7 @@ public class TutorialController : MonoBehaviour
             nextButton.interactable = false;
             exitButton.interactable = true;
         }
-        //Disable the player's last-second dodging if not in the lastSecondDodgeTest state.
+        //Disable the player's last-second dodging & the LSDVisualizer if not in the LSDTest state.
         else
         {
             LSD.enabled = false;
