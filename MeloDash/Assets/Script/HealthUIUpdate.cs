0abf6281b29fb19119309cc91f65b4f91831324b
Aditya Vashishtha <avtmax007@gmail.com>
2024-04-06 20:36:14 -0400

    FixingUI

--- MeloDash/Assets/Script/HealthUIUpdate.cs
+++ MeloDash/Assets/Script/HealthUIUpdate.cs
@@ -29,15 +29,15 @@ public class HealthUIUpdate : MonoBehaviour
         Invoke("endGame", 125);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(playerHealth.healthNum <= 0 && loader != null)
+        if (playerHealth.healthNum <= 0 && loader != null)
         {
             loader.loadScene("GameOverScreen");
         }
 
-        if(regenBar == null)
+        // Add null check for regenBar
+        if (regenBar != null)
         {
             regenBar.fillAmount = playerHealth.currentTime / playerHealth.regenTime;
         }
